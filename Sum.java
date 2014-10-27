b2f36ceeab25522226c175640d755c4f2aab6aa7
tsuhovenko <tsuhovenko@cogniance.com>
2014-10-27 12:06:44 +0200

    fix

--- Sum.java
+++ Sum.java
@@ -2,12 +2,17 @@ class Sum {
     public static void main(String[] args) {
         if (args.length != 0) {
             int sum = 0;
-            for (String number: args) {
-                try {
-                    sum += Integer.parseInt(number);
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid number: " + number);
-                    return;
+            for (String arg: args) {
+                String[] numbers = arg.split(" "); 
+                for (String number: numbers) {
+                    try {
+                        if (number.length() != 0) {
+                            sum += Integer.parseInt(number);
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid number: " + number);
+                        return;
+                    }
                 }
             }
             System.out.println("Result: " + sum);
