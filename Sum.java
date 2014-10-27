365fd2ec9228c6c5b9be267f957128fb89a806e7
tsuhovenko <tsuhovenko@cogniance.com>
2014-10-27 12:17:46 +0200

    refactor

--- Sum.java
+++ Sum.java
@@ -1,23 +1,23 @@
 class Sum {
     public static void main(String[] args) {
-        if (args.length != 0) {
-            int sum = 0;
+        if (args.length == 0) {
+            System.out.println("Please, type any number");
+            return;
+        }
+        int sum = 0;
+        try {
             for (String arg: args) {
                 String[] numbers = arg.split(" "); 
                 for (String number: numbers) {
-                    try {
-                        if (number.length() != 0) {
-                            sum += Integer.parseInt(number);
-                        }
-                    } catch (NumberFormatException e) {
-                        System.out.println("Invalid number: " + number);
-                        return;
+                    if (number.length() != 0) {
+                        sum += Integer.parseInt(number);
                     }
                 }
             }
-            System.out.println("Result: " + sum);
-        } else {
-            System.out.println("Please, type any number");
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number: " + e);
+            return;
         }
+        System.out.println("Result: " + sum);
     }
 }
